5781e39f6e75832f75dabd3614e87d4ccd1e9432
thatavieira <thagurgel0909@gmail.com>
2023-03-20 17:04:44 -0300

    Introduction

---
+++ app-expo/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {View, Text} from 'react-native';
+import HomePage from './src/pages/homepage'
+
+//function
+const App = () => {
+
+return (
+  <HomePage/>
+);
+
+}
+
+export default App;
---
+++ app-expo/src/pages/homepage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {View, Text} from 'react-native';
+
+//function
+const HomePage = () => {
+
+return (
+  <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+  
+    <Text> PUC MINAS </Text>
+  
+  </View>
+);
+
+}
+
+export default HomePage;
